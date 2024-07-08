0c2f0c0bccfaef89715132902541565a416d1af4
naveen <stupiddint@gmail.com>
2024-07-08 21:07:58 +0530

    add: data insetion logic

---
+++ php/create.php
@@ -0,0 +1,22 @@
+<?php
+
+if($conn){
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
+        // echo "server made resquested 'POST' method";
+        $description = $_POST["desc"];
+        // echo var_dump($description);
+        if(!empty($description)){
+            $sql = "INSERT INTO `todo` (`description`) VALUES ('$description')";
+            $result = mysqli_query($conn, $sql);
+
+            if($result){
+                echo '<script> alert("successfully inserted"); </script>';
+                header('Location: success.php');
+                header('Location: index.php');
+            }
+            // echo var_dump($result);
+        }
+    }
+}
+
+?>
