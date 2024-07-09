6b90170107d93aa3bc2b4a23f1bd867e8213a1d7
naveen <stupiddint@gmail.com>
2024-07-09 20:17:12 +0530

    chore: code restructure

--- php/config-example.php
+++ php/config-example.php
@@ -12,11 +12,6 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 if(!$conn){
     $connectionMsg = "sorry we failed to connect: ". mysqli_connect_error();
 }else{
-    echo '<script>
-            setTimeout(function() {
-                document.getElementById("successMessage").style.display = "none";
-            }, 1000); // 1000 milliseconds = 1 second
-          </script>';
     $connectionMsg = "Database connected successfully";
 }
 
